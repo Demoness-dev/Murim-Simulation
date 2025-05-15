da9f18cbafcc6bb6ea74a0f061a00371098243dd
Guilherme <dopsks3@gmail.com>
2025-05-15 20:07:39 -0300

    minor adjustments

--- import_recs.py
+++
@@ -1,7 +0,0 @@
-from martial_artist_definition import MartialArtist
-from city import City
-from sect import Sect
-from build import Building
-from battle_manager import manage_brackets
-from console_writer import log
-from logger import logger
